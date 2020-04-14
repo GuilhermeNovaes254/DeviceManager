93f13b35bbc9410d48d6329d6f5972fbea5a4308
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-04-14 16:44:15 -0300

    Estrutura Header e Painel finalizada

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -2,6 +2,7 @@ function openNav() {
   document.getElementById("mySidebar").style.width = "200px";
   document.getElementById("main").style.marginLeft = "200px";
   document.getElementById("myHeader").style.marginLeft = "200px";
+  document.getElementById("myHeader").style.marginRight = "0px";
 
 }
 
@@ -10,5 +11,6 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
   document.getElementById("myHeader").style.marginLeft = "0";
+  document.getElementById("logoHeader").style.marginRight = "0";
 }
 
